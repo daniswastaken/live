a17aabf289929cea214975c5d11d91020b22e658
daniswastaken <daniswastaken@gmail.com>
2024-04-30 19:04:07 +0700

    Setup

---
+++ index.js
@@ -0,0 +1,17 @@
+// Requiring necessary package
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Processing TOKEN from .env
+const TOKEN = process.env.TOKEN;
+
+// When client ready test code
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Success. Logged in as ${readyClient.user.tag} => }`)
+})
+
+// Login to Discord using TOKEN
+client.login(TOKEN);
