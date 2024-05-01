14cb621dfddb0c2dad7c05301b9aae7d35ee0989
daniswastaken <daniswastaken@gmail.com>
2024-05-01 17:48:05 +0700

    fixing crash

--- index.js
+++ index.js
@@ -10,25 +10,6 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // Processing TOKEN from .env
 const TOKEN = process.env.TOKEN;
 
-
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
-}
-
 // When client ready test code
 client.once(Events.ClientReady, readyClient => {
     console.log(`Success. Logged in as ${readyClient.user.tag} => }`)
