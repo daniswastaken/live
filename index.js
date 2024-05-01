391fefea74a603e5e79b7dd81848348140c68189
daniswastaken <daniswastaken@gmail.com>
2024-05-01 14:38:07 +0700

    i have no idea what is this

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Requiring necessary package
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 // Create a new client instance
@@ -7,6 +9,25 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // Processing TOKEN from .env
 const TOKEN = process.env.TOKEN;
+
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
 
 // When client ready test code
 client.once(Events.ClientReady, readyClient => {
